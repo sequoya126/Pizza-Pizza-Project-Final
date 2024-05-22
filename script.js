49f0f55e263e62b84f66cb34a3f235508ce93eeb
so123456789 <106389045+so123456789@users.noreply.github.com>
2024-05-22 17:59:56 +0000

    22/05

--- script.js
+++ script.js
@@ -1,54 +1,29 @@
-let items = [
-    {
-        index: 1,
-        quan: 0,
-        nom: "Souris",
-        image: "Images/Souris.png",
-        prix: 50
-    },
-    {
-        index: 2,
-        quan: 0,
-        nom: 'Clavier',
-        image: "Images/clavier.png",
-        prix: 150
-    },
-    {
-        index: 3,
-        quan: 0,
-        nom: 'ecran',
-        image: "Images/ecran.png",
-        prix: 300
-    },
-    {
-        index: 4,
-        quan: 0,
-        nom: 'Speakers',
-        image: "Images/Speakers.png",
-        prix: 20
-    },
-    {
-        index: 5,
-        quan: 0,
-        nom: 'Tour',
-        image: "Images/la tour.png",
-        prix: 200
-    },
-    {
-        index: 6,
-        quan: 0,
-        nom: 'Laptop',
-        image: "Images/laptop.png",
-        prix: 1000
-    }
+// Les liens entre les items de HTML et JavaScript
+let itemList = document.querySelector('.items');
+localStorage.setItem('panierCourses', JSON.stringify(listStorage));
+
+
+const items = [
+    { nom: "Create Your Own", prix: 10, image: "create_your_own_2.jpg", index: 0 },
+    { nom: "Meat Lover", prix: 12, image: "old_school_2.jpg", index: 1 },
+    // 
 ];
 
+const listStorage = []; 
+
+function AjouterAuPanier(index, itemIndex) {
+    // 
+}
+
 function initItem() {
+    const totals = document.getElementById('totals');
+    const itemList = document.getElementById('itemList');
+
     totals.innerHTML = listStorage.length;
-    // Clear any existing items in the display
+    //
     itemList.innerHTML = '';
 
-    // Loop through each item and create its representation
+    // L
     for (let i = 0; i < items.length; i++) {
         let value = items[i];
         let card = document.createElement('div');
@@ -57,12 +32,11 @@ function initItem() {
             <img src="${value.image}" class="card-img-top" alt="...">
             <div class="nom-prix-bouton"> 
                 <h4 class="cart-h4">${value.nom} ${value.prix}$</h4>
-                <button class="AjouterAuPanier" id = "${i}" onclick="AjouterAuPanier(${i}, ${value.index})">Ajouter au panier</button>
+                <button class="AjouterAuPanier" id="${i}" onclick="AjouterAuPanier(${i}, ${value.index})">Ajouter au panier</button>
             </div>`;
-        // Append the item representation to the items container
+        // 
         itemList.appendChild(card);
-    //     btns = document.querySelectorAll("AjouterAuPanier")
     }
-    
 }
+
 initItem();
